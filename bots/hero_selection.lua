c1853f0820e7f1e28195cae74b0e687d9cbca457
Krey43 <piotr95pietrzak@gmail.com>
2017-01-27 19:06:09 +0100

    Add files via upload

--- bots/hero_selection.lua
+++ bots/hero_selection.lua
@@ -1,22 +1,31 @@
-
-
-local RadiantBots = { "npc_dota_hero_antimage", "npc_dota_hero_axe", "npc_dota_hero_bane", "npc_dota_hero_slark", "npc_dota_hero_crystal_maiden" };
-local DireBots    = { "npc_dota_hero_drow_ranger", "npc_dota_hero_earthshaker", "npc_dota_hero_juggernaut",  "npc_dota_hero_mirana", "npc_dota_hero_nevermore" };
-
+local MyBots={"npc_dota_hero_crystal_maiden", "npc_dota_hero_templar_assassin", "npc_dota_hero_bristleback", "npc_dota_hero_legion_commander", "npc_dota_hero_slark"};
 
 function Think()
-    Team = GetTeam();
-    if Team == TEAM_RADIANT then Bots = RadiantBots;
-                            else Bots = DireBots;
-                            end
-                            
-    local IDs = GetTeamPlayers( Team );
-	
-    for i, id in pairs( IDs ) do
-        if IsPlayerBot( id ) 
-        then 
-			SelectHero( id, Bots[i] );
-        end
-    end
+	local IDs=GetTeamPlayers(GetTeam());
+	for i,id in pairs(IDs) do
+		if IsPlayerBot(id) then
+			SelectHero(id,MyBots[i]);
+		end
+	end
+
 end
 
+function UpdateLaneAssignments()
+	if ( GetTeam() == TEAM_RADIANT ) then
+		return {
+			[1] = LANE_TOP,
+			[2] = LANE_MID,
+			[3] = LANE_BOT,
+			[4] = LANE_TOP,
+			[5] = LANE_BOT,
+		};
+	elseif ( GetTeam() == TEAM_DIRE ) then
+		return {
+			[1] = LANE_BOT,
+			[2] = LANE_BOT,
+			[3] = LANE_TOP,
+			[4] = LANE_BOT,
+			[5] = LANE_MID,
+		};
+	end
+end
