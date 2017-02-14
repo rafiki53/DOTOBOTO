d6d473996c58094bb42be163ea05b9aab1dd00f9
rafiki53 <jacek.moszkowski@gmail.com>
2017-02-14 23:25:30 +0100

    Update ability_item_usage_legion_commander.lua

--- bots/ability_item_usage_legion_commander.lua
+++ bots/ability_item_usage_legion_commander.lua
@@ -25,45 +25,33 @@ function AbilityUsageThink()
 	
 	--local Score=0;
 	enemy = npcBot:GetTarget();
-	if(enemy == nil or not enemy:IsHero()) then
-		return;
-	end
-	--[[,Score=FindTarget();
-	if Target==nil or npcBot:GetHealth()/npcBot:GetMaxHealth() < 0.37  or Score < 0.5 then
-		isRoaming=false;
-		npcBot.isRoaming=false;
-		return;
-	end
-	]]--
-	local aw=npcBot:GetAbilityByName(Abilities[2]);
-	local ar=npcBot:GetAbilityByName(Abilities[4]);
-	local blink = Utility.IsItemAvailable("item_blink");
-	
-	local dist=GetUnitToUnitDistance(enemy,npcBot);
-	
-	local cb = (blink ~= nil and blink:IsFullyCastable())
-	local cw = (aw ~= nil and aw:IsFullyCastable())
-	local cr = (ar ~= nil and ar:IsFullyCastable())
-	
-	print("cb ", cb)
-	print("cw ", cw)
-	print("cr ", cr)
-	print("dist ", dist)
-	
-	if cb and cw and cr and (dist < 1200) and not npcBot:HasModifier("modifier_legion_commander_press_the_attack") then
-		npcBot:Action_UseAbilityOnEntity(aw,npcBot);
-		return;
-	end	
-	
-	if cb and cr and (dist < 1200) and npcBot:HasModifier("modifier_legion_commander_press_the_attack") then
-		npcBot:Action_UseAbilityOnLocation(blink, enemy:GetLocation())
-		return;
+	if(enemy ~= nil and enemy:IsHero()) then
+		local aw=npcBot:GetAbilityByName(Abilities[2]);
+		local ar=npcBot:GetAbilityByName(Abilities[4]);
+		local blink = Utility.IsItemAvailable("item_blink");
+
+		local dist=GetUnitToUnitDistance(enemy,npcBot);
+
+		local cb = (blink ~= nil and blink:IsFullyCastable())
+		local cw = (aw ~= nil and aw:IsFullyCastable())
+		local cr = (ar ~= nil and ar:IsFullyCastable())
+
+		if cb and cw and cr and (dist < 1200) and not npcBot:HasModifier("modifier_legion_commander_press_the_attack") then
+			npcBot:Action_UseAbilityOnEntity(aw,npcBot);
+			return;
+		end	
+
+		if cb and cr and (dist < 1200) and npcBot:HasModifier("modifier_legion_commander_press_the_attack") then
+			npcBot:Action_UseAbilityOnLocation(blink, enemy:GetLocation())
+			return;
+		end
+
+		if cr and dist < 150 then
+			npcBot:Action_UseAbilityOnEntity(ar,enemy);
+			return;
+		end
 	end
 	
-	if cr and dist < 150 then
-		npcBot:Action_UseAbilityOnEntity(ar,enemy);
-		return;
-	end
 
 	if npcBot:GetMaxHealth() - npcBot:GetHealth() > 200 then
 		UseW();
